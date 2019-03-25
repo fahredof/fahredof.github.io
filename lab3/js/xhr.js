c280a881c1ea1a86f8c5dc72eb295deecffe92ed
Arthur Fakhretdinov <turik20121998@gmail.com>
2019-03-25 13:03:41 +0300

    LAB3: add convas & one pic in

--- lab3/js/xhr.js
+++ lab3/js/xhr.js
@@ -1,3 +1,9 @@
+arrphotos = new Array();
+var index = 0;
+
+for (var i = 0; i < 4; i++) {
+  arrphotos[i] = new Image();
+}
 
 $.ajax({
       url: "https://api.forismatic.com/api/1.0/",
@@ -12,3 +18,40 @@ $.ajax({
     .done(function(data) {
     	document.getElementById('text').innerHTML = data.quoteText ;
 	})
+
+
+function getphotos(key){
+  $.ajax({
+        url: "https://api.unsplash.com/photos/random",
+        data: {
+        	client_id: '18f07c2c511e7d347e6bc3995eba8ebf75fcbe9b79687038017b14bf83ac5d7f'
+        }
+      })
+        .done(function(data) {
+        document.getElementById('img').innerHTML+= '<img src="' + data.urls.small + '" />';
+        arrphotos[index].src = data.urls.small;
+        index++;
+  	})
+}
+
+$(function(){
+  var canvas=document.getElementById("c1")
+  var image1=canvas.getContext("2d");
+
+  getphotos();
+
+  arrphotos[0].onload = function() {
+    image1.drawImage(arrphotos[0], 0, 0, 300, 200);
+  }
+});
+/*
+function drawtx(){
+  var canvas=document.getElementById("c1")
+  var text=canvas.getContext("2d");
+  text.font='28px Verdana';
+  text.fillStyle='#82216f';
+  text.fillText("Вставка текста в Canvas", 20, 100);
+}
+
+drawtx();
+*/
